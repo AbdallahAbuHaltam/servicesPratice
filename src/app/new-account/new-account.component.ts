3d697591e7e9c5a8373e4e89ca70d0609d15f65f
Abdullah AbuHaltam <abdullahhaltam2@gmail.com>
2023-10-16 16:46:19 +0300

    Added New Account

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-new-account',
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./new-account.component.css']
 })
 export class NewAccountComponent {
+  accountAdded=new EventEmitter<{name:string,status:string}>();
 
+  onCreateAccount(accountName:string,accountStatus:string){
+    this.accountAdded.emit({
+      name: accountName,
+      status: accountStatus
+     });
+  }
 }
